bfe7046237f93c51b5b5131f230b7a13700d10d1
samWson <samWson@users.noreply.github.com>
2017-05-23 21:32:16 +1200

    Add project files.

---
+++ battery-log/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace battery_log
+{
+    [Activity(Label = "battery_log", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
